2715f8b24a4a2b05adb8b7abf40ab59954fbe0c2
Aylla Scaglia <aylla@scaglia.com.br>
2024-08-08 01:43:13 -0300

    feat: add BookingItem component

---
+++ src/app/_constants/search.ts
@@ -0,0 +1,22 @@
+interface QuickSearchOptions {
+   imageUrl: string;
+   title: string
+}
+  
+export const quickSearchOptions: QuickSearchOptions[] = [{
+   imageUrl: "/cabelo.svg",
+   title: "Cabelo"
+},{
+   imageUrl: "/barba.svg",
+   title: "Barba"
+}, {
+   imageUrl: "/acabamento.svg",
+   title: "Acabamento"
+}, {
+   imageUrl: "/sobrancelha.svg",
+   title: "Sobrancelha"
+}, {
+   imageUrl: "/cabelo.svg",
+   title: "Fazer o pé"
+}]
+  
